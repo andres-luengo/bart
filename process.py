d3448afec198a6e8b709ff19c2b912c3984c0c06
andres-luengo <andres.luengo@yale.edu>
2025-07-10 13:39:55 -0700

    testing testing testing

--- process.py
+++ process.py
@@ -5,14 +5,13 @@ from pathlib import Path
 
 import logging
 
-from threading import Thread
-from queue import Queue, ShutDown
+COLUMNS = ('file',)
 
 class PickleJob:
     def __init__(
             self, 
             outdir: Path, 
-            batch: list[str], 
+            batch: tuple[Path, ...], 
             batch_num: int = -1,
     ):
         self._logger = logging.getLogger(__name__)
@@ -20,20 +19,14 @@ class PickleJob:
         self.batch = batch
         self.batch_num = batch_num
 
-        self.feature_table = pd.DataFrame(columns = [
-            'frequency', 'file'
-        ])
-
-        # if you have more than 999 batches, you have bigger problems than the file names being out of order
         self.save_path = outdir / f'batch_{batch_num:>03}.csv'
         self._logger.info(f'Creating {self.save_path}')
-        self.feature_table.to_csv(self.save_path, index = False)
+        pd.DataFrame(columns = COLUMNS).to_csv(self.save_path)
     
     def run(self):
-        self._logger.info(f'Running on batch {self.batch}')
-        return self.feature_table
-
-def save_thread(data_queue: Queue):
-    data_buffer = []
-    while True:
-        
+        self._logger.info(f'Running on batch {self.batch_num}.')
+        self._logger.debug(f'That is, {self.batch = }')
+        self.process_file(self.batch[0])
+    
+    def process_file(self, file: Path):
+        raise ValueError('explode :(')
